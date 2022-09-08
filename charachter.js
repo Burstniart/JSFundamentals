3497b7f82da4d9656e5fe8f278f7ea1d2434101c
Burstniart <v4gr33n@hotmail.com>
2022-09-07 20:26:42 -0500

    I remember

---
+++ charachter.js
@@ -0,0 +1,17 @@
+console.log("Create character objectse")
+
+var game = {};
+
+game.murder = "???";
+
+game['weapons'] = [
+    'lasers',
+    'angry cats',
+    'oblivious stare'
+];
+
+game.name = [];
+game.name[0] = 'Miss Scarlet';
+game.name.push('Mr. Green');
+
+console.log(game.name);
