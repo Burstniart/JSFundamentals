f6c74505cac780db8f887cb1076d22387fcfbdd2
Burstniart <v4gr33n@hotmail.com>
2022-09-08 19:33:56 -0500

    Daily commit

--- charachter.js
+++ charachter.js
@@ -15,3 +15,11 @@ game.name[0] = 'Miss Scarlet';
 game.name.push('Mr. Green');
 
 console.log(game.name);
+
+const {name, weapon, room} = {
+    name: 'Rusty',
+    room: 'kitchen',
+    weapon: 'candlestick'
+}
+
+console.log(room, weapon)
