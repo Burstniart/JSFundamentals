2e2998e8c69583e692cf9177214e52821bf4ff30
Burstniart <v4gr33n@hotmail.com>
2022-09-10 23:45:20 -0500

    halfway there

--- charachter.js
+++ charachter.js
@@ -1,3 +1,4 @@
+
 console.log("Create character objectse")
 
 var game = {};
---
+++ clue.js
@@ -0,0 +1,102 @@
+const videoData = [
+    {
+        name: 'Miss Scarlet',
+        present: true,
+        rooms: [
+            {kitchen: false},
+            {ballroom: false},
+            {conservatory: false},
+            {'dining room': false},
+            {'billiard room': false},
+            {library: false}
+        ]
+    },
+    {
+        name: 'Mrs. White',
+        present: false,
+        rooms: [
+            {kitchen: false},
+            {ballroom: false},
+            {conservatory: false},
+            {'dining room': false},
+            {'billiard room': false},
+            {library: false}
+        ]
+    },
+    {
+        name: 'Reverend Green',
+        present: true,
+        rooms: [
+            {kitchen: false},
+            {ballroom: false},
+            {conservatory: false},
+            {'dining room': false},
+            {'billiard room': false},
+            {library: false}
+        ]
+    },
+    {
+        name: 'Rusty',
+        present: false,
+        rooms: [
+            {kitchen: false},
+            {ballroom: false},
+            {conservatory: false},
+            {'dining room': false},
+            {'billiard room': false},
+            {library: false}
+        ]
+    },
+    {
+        name: 'Colonel Mustard',
+        present: true,
+        rooms: [
+            {kitchen: false},
+            {ballroom: false},
+            {conservatory: false},
+            {'dining room': false},
+            {'billiard room': false},
+            {library: false}
+        ]
+    },
+    {
+        name: 'Professor Plum',
+        present: true,
+        rooms: [
+            {kitchen: false},
+            {ballroom: false},
+            {conservatory: false},
+            {'dining room': false},
+            {'billiard room': false},
+            {library: false}
+        ]
+    }
+];
+
+
+const _ = {};
+
+_.filter = function(arr, cb) {
+    const storage = [];
+    for (let i = 0; i < arr.length; i++) {
+        if(cb(arr[i], i, arr) === true) {
+            storage.push(arr[i]);
+        }
+    }
+    return storage;
+}
+
+//outside function 
+// const wasThere = function (sus) {
+//     return sus.present;
+// }
+
+// var suspect = _.filter(videoData, wasThere);
+
+//filter callling
+var suspect = _.filter(videoData, function(sus) {
+    return sus.present;
+})
+
+console.log(suspect);
+
