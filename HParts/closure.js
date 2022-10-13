4b3813cc944e8e1f1601d116bb2e5afc771db7fa
Burstniart <v4gr33n@hotmail.com>
2022-10-12 23:02:44 -0500

    Closure is confusing, remember context

--- HParts/closure.js
+++ HParts/closure.js
@@ -9,6 +9,12 @@ function createFunction() {
 // Challenge 2
 // Create a function createFunctionPrinter that accepts one input and returns a function. When that created function is called, it should print out the input that was used when the function was created.
 
+function createFunctionPrinter (input) {
+        function myPrinter(myInput) {
+                    console.log(input);
+                }
+        return myPrinter;
+}
 // Challenge 3
 // Examine the code for the outer function. Notice that we are returning a function and that function is using variables that are outside of its scope.
 // Now we are going to create a function addByX that returns a function that will add an input by x.
@@ -28,12 +34,19 @@ function addByX(x) {
     }
     return addedByX;
 }
-//
-function createFunctionPrinter (input) {
-    function myPrinter(myInput) {
-        console.log(input);
+
+// Challenge 4
+// Write a function once that accepts a callback as input and returns a function. When the returned function is called the first time, it should call the callback and return that output. If it is called any additional times, instead of calling the callback again it will simply return the output value from the first time it was called.
+
+function once(callback) {
+    let counter = 0;
+    let onceVal;
+    const innerFunc = val => {
+        if (counter === 0) onceVal = callback(val);
+        counter++;
+        return onceVal;
     }
-    return myPrinter;
+    return innerFunc;
 }
 //
 import { createInterface } from 'readline';
@@ -42,6 +55,13 @@ const rl = createInterface ({
     output: process.stdout
 });
 
+/*C4
+const addByTwo = addByX(2);
+const onceFunc = once(addByTwo);
+console.log(onceFunc(4));
+console.log(onceFunc(6));
+rl.close();
+*/
 /*C3
 // return addedByx function, remember that
 const addByTwo = addByX(2);
