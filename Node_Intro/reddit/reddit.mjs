912f3aa21f8258f934dfb3d5c693887b927759c1
Burstniart <v4gr33n@hotmail.com>
2022-10-17 18:11:23 -0500

    Now the cli implementation of calling reddit works :)

---
+++ Node_Intro/reddit/reddit.mjs
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+import open from 'open'
+import fetch from 'node-fetch'
+import yargs from 'yargs'
+
+const {argv} = yargs(process.argv)
+
+const res = await fetch('https://www.reddit.com/.json')
+const data = await res.json()
+
+console.log(data)
