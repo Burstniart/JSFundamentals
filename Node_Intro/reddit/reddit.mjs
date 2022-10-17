e9900935174385542fde8c4dfccfaba3a9b6317a
Burstniart <v4gr33n@hotmail.com>
2022-10-17 18:37:55 -0500

    CLI tool

--- Node_Intro/reddit/reddit.mjs
+++ Node_Intro/reddit/reddit.mjs
@@ -8,4 +8,18 @@ const {argv} = yargs(process.argv)
 const res = await fetch('https://www.reddit.com/.json')
 const data = await res.json()
 
-console.log(data)
+//console.log(data)
+
+const randomIndex = Math.floor(Math.random() * data.data.children.length)
+const post = data.data.children[randomIndex]
+
+// log if --print flag is passed
+if (argv.print) {
+    console.log(`
+        Title: ${post.data.title}\n
+        Link: ${post.data.permalink}
+        `)
+} else {
+    // open in browser if not
+    open(`https://reddit.com${post.data.permalink}`)
+}
